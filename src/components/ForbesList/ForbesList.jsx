fab4e672c0936aa9fb680c19b498307a94f7b107
Artem Zakharchuk <zakharchukartem18@gmail.com>
2025-05-10 21:17:07 +0300

    New changes!

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,5 +1,38 @@
-const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import PropTypes from "prop-types";
+import ForbesListItem from "../ForbesListItem/ForbesListItem";
+import style from "./ForbesList.module.css";
+
+const ForbesList = ({ list }) => {
+  return (
+    <div className={style.board}>
+      <div className={style.header}>
+        <h2 className={style.title}>
+          <span className={style.titleTop}>Forbes</span>
+          <span className={style.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={style.list}>
+        {list.map(({ id, name, avatar, capital, isIncrease }) => (
+          <li key={id} className={style.item}>
+            <ForbesListItem name={name} avatar={avatar} capital={capital} isIncrease={isIncrease} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+ForbesList.PropTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      capital: PropTypes.number.isRequired,
+      isIncrease: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
 };
 
 export default ForbesList;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,42 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import  StatisticsItem  from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import PropTypes from 'prop-types';
+import { FaRegThumbsUp } from 'react-icons/fa';
+ import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+ import { GiTreeDoor } from 'react-icons/gi';
+import style from './Statistics.module.css';
+ 
+const iconMap = {
+  'Happy Customers': <FaRegThumbsUp size={32} />,
+  'Registered Members': <MdPeople size={32} />,
+  'Available Products': <MdOutlineProductionQuantityLimits size={32} />,
+  'Saved Trees': <GiTreeDoor size={32} />,
+};
 
-const Statistics = () => {
-  return <h2>Statistics</h2>;
+const Statistics = ({ title, stats }) => {
+  return (
+    <>
+      {title && <h3 className={style.title}>{title}</h3>}
+      <ul className={style.list}>
+        {stats.map(({ id, title, total }) => (
+          <li key={id} className={style.item}>
+            {iconMap[title]}
+            <span className={style.counter}>{total}</span>
+            <p className={style.text}>{title}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      total: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Statistics;
